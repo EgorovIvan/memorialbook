04a6d6609cd0c132f0a478706e8b1dab02d61e58
Maxim Okolot <maximokolot@gmail.com>
2023-02-18 12:00:20 +0300

    add page qr profile

--- js/script.js
+++ js/script.js
@@ -896,4 +896,36 @@
 
   }
 
+
+  window.addEventListener('load', function () {
+    //слайдеры карточек
+    let qrCodeSlider = document.querySelector('.qr-code-slider');
+
+    if (qrCodeSlider) {
+
+      let swiper = new Swiper(qrCodeSlider, {
+        slidesPerView: 2,
+        spaceBetween: 30,
+        navigation: {
+          nextEl: '.qr-code-slider__next',
+          prevEl: '.qr-code-slider__prev',
+        }
+      });
+
+
+      let btnGrid = document.querySelector('.btn-style-grid');
+      let btnInline = document.querySelector('.btn-style-inline');
+      let gallery = document.querySelector('.member-images');
+
+      btnGrid.addEventListener('click', function () {
+        gallery.classList.remove('no-grid');
+      })
+
+      btnInline.addEventListener('click', function () {
+        gallery.classList.add('no-grid');
+      })
+
+    }
+  })
+
 })();
